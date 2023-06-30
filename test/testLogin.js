7fde7ac8512aa7fae8097fe20bd9a4e7e03c1bba
jzafvt771 <ijmm7171@gmail.com>
2023-06-29 21:04:39 -0500

    test Cases

--- test/testLogin.js
+++ test/testLogin.js
@@ -8,17 +8,11 @@ async function test_case() {
   let btnLog = await driver.findElement(By.id('btnLogNav'));
   let btnRegister = await driver.findElement(By.id('btnRegisterNav'));
 
-  // await btnLog.click();
-  // console.log('Se hizo clic en el botón de Log In');
-
-  // await driver.sleep(1000); // Esperar 1 segundo
-
-  //  // Regresar a la página anterior
-
-  await btnRegister.click();
-  console.log('Se hizo clic en el botón de Register');
+  await btnLog.click();
+  console.log('Se hizo clic en el botón de Log In');
 
   await driver.sleep(1000); // Esperar 1 segundo
+
   await driver.navigate().back();
   await driver.quit();
 }
