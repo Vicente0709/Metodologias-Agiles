137071e7c300f94fdfd4fb43b1046aa03be16ebb
jzafvt771 <ijmm7171@gmail.com>
2023-09-10 22:32:44 -0500

    front + back

---
+++ frontend/js/reseteo.js
@@ -0,0 +1,47 @@
+// Initialize Firebase
+var firebaseConfig = {
+    // Aquí debes agregar la configuración de tu proyecto Firebase
+    apiKey: "AIzaSyAq3PWrGxMVqAcvfXwpC21vzsSQlVI-AdM",
+    authDomain: "polimarket-93916.firebaseapp.com",
+    projectId: "polimarket-93916",
+    storageBucket: "polimarket-93916.appspot.com",
+    messagingSenderId: "624045411960",
+    appId: "1:624045411960:web:55537b983ead1f3516e78d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Obtener referencias a los elementos del formulario
+var emailInput = document.getElementById("email");
+
+// Agregar un listener al formulario para capturar el evento de envío
+document.getElementById("formulario").addEventListener("submit", function (e) {
+  e.preventDefault(); // Evitar el envío del formulario por defecto
+
+  var email = emailInput.value.trim();
+
+  // Validar que el correo tenga un formato válido
+  var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido.");
+    return; // No continuar si el correo no es válido
+  }
+
+  // Llama a la función de Firebase para restablecer la contraseña
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(function () {
+      // Restablecimiento de contraseña exitoso
+      alert(
+        "Se ha enviado un correo electrónico para restablecer la contraseña."
+      );
+      window.location.href = "login.html";
+    })
+    .catch(function (error) {
+      // Ocurrió un error durante el restablecimiento de contraseña
+      var errorMessage = error.message;
+      alert("Error al restablecer la contraseña: " + errorMessage);
+    });
+});
